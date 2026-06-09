90ee2112cbcbfbf2c442c1191027614f5200cd21
daksara <daksara.dev@gmail.com>
2026-06-09 14:56:50 +0700

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState, type ReactNode } from 'react'
-import { auth, getProfile, type Profile } from '../lib/firebase'
+import { auth, type Profile } from '../lib/firebase'
 import { onAuthStateChanged, type User } from 'firebase/auth'
 import { doc, onSnapshot } from 'firebase/firestore'
 import { db } from '../lib/firebase'
